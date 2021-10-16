9446c96d7e429ef69ad24422567a60797019e69b
Andria Kilasonia <Andria@MACBOOKs-MacBook-Pro.local>
2021-10-16 21:29:50 +0400

    add solution to the repo

--- Project_1/Project_1/ViewController.swift
+++ Project_1/Project_1/ViewController.swift
@@ -9,9 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var displays: [UIView]!
+    
+    let displayNumber = 700
+    let onAlpha = 1.0
+    let offAlpha = 0.2
+    
+    /*
+     * this array stores 1 for the segment indexes that need to light up.
+     * orderd from top-left (North first, bottom last)
+     *
+     * Bool array needed more writing, writing 1 and 0 is faster
+     */
+    let numberSegments: [[Int]] = [
+        [1, 1, 1, 0, 1, 1, 1], // 0
+        [0, 0, 1, 0, 0, 1, 0], // 1
+        [1, 0, 1, 1, 1, 0, 1], // 2
+        [1, 0, 1, 1, 0, 1, 1], // 3
+        [0, 1, 1, 1, 0, 1, 0], // 4
+        [1, 1, 0, 1, 0, 1, 1], // 5
+        [1, 1, 0, 1, 1, 1, 1], // 6
+        [1, 0, 1, 0, 0, 1, 0], // 7
+        [1, 1, 1, 1, 1, 1, 1], // 8
+        [1, 1, 1, 1, 0, 1, 1]  // 9
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let hundredsDigit = displayNumber / 100
+        let tensDigit = displayNumber / 10 % 10
+        let onesDigit = displayNumber % 10
+        let displayNumberDigits = [hundredsDigit, tensDigit, onesDigit]
+        
+        var numDisplay = 0
+        for display in displays {
+            var i = 0
+            for segment in display.subviews {
+                if numberSegments[displayNumberDigits[numDisplay]][i] == 1 {
+                    segment.alpha = onAlpha
+                } else {
+                    segment.alpha = offAlpha
+                }
+                    
+                i += 1
+            }
+            numDisplay += 1
+        }
     }
 
 }
