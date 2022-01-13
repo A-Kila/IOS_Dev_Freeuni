bebe777e2ee04f7a23eb6ccd1f04955b8e606b8c
Andria Kilasonia <akila19@freeuni.edu.ge>
2022-01-13 13:15:26 +0400

    initilize project_5, contact book

---
+++ Project_5/Project_5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project_5
+//
+//  Created by Andria Kilasonia on 13.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
