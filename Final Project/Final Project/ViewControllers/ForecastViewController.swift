1d7689ac32667516e4a7322215708cdade382e1a
Andria Kilasonia <akila19@freeuni.edu.ge>
2022-02-13 19:50:51 +0400

    added navigation to the project

--- Final Project/Final Project/ViewControllers/ForecastViewController.swift
+++ Final Project/Final Project/ViewControllers/ForecastViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ForecastViewController.swift
 //  Final Project
 //
 //  Created by Andria Kilasonia on 13.02.22.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ForecastViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
---
+++ Final Project/Final Project/ViewControllers/TodayViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TodayViewController.swift
+//  Final Project
+//
+//  Created by Andria Kilasonia on 13.02.22.
+//
+
+import UIKit
+
+class TodayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
