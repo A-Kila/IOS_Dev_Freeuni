46f27283eab7aebd930e328ad25ca5fc29c597e8
Andria Kilasonia <akila19@freeuni.edu.ge>
2022-02-13 17:29:13 +0400

    create final project

---
+++ Final Project/Final Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Final Project
+//
+//  Created by Andria Kilasonia on 13.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
